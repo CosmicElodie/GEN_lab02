77ac7c793dd23b80ce71f09dbc92070181eebf69
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-03-26 23:21:11 +0100

    p.30, N.1-4, fixes #6

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -11,8 +11,6 @@ class DollarTest
         assertEquals(new Dollar(15), five.times(3));
     }
 
-
-
     @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
