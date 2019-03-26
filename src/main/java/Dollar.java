6aa09711466e35c072fd99bbe5e3d19aa0b29216
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-03-26 22:16:23 +0100

    p.21, N.1-4, fixes #1

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -3,10 +3,11 @@ public class Dollar
     int amount;
 
     Dollar(int amount) {
+        this.amount= amount;
     }
 
     void times(int multiplier) {
-        amount= 5 * 2;
+        amount *= multiplier;
     }
 
 
