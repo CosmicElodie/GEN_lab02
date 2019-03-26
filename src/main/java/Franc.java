1ebd33035e2d4965a05290a5114193cc42143924
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-03-27 00:51:41 +0100

    p.45, N.2, issue #12

--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -5,7 +5,7 @@ public class Franc extends Money{
     }
 
     Money times(int multiplier) {
-        return Money.franc(amount * multiplier);
+        return new Franc(amount * multiplier, "CHF");
     }
 
 }
