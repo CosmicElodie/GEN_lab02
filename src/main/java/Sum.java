c0d45ef3703a45c3a8806573d8b33c4f72878ba7
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-03-27 11:25:05 +0100

    p.66, N.2-4, fixes #16

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -13,4 +13,8 @@ public class Sum implements Expression {
         return new Money(amount, to);
     }
 
+    public Expression plus(Expression addend) {
+        return null;
+    }
+
 }
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -79,7 +79,7 @@ class MoneyTest
 
     @Test
     public void testMixedAddition() {
-        Money fiveBucks= Money.dollar(5);
+        Expression fiveBucks= Money.dollar(5);
         Expression tenFrancs= Money.franc(10);
         Bank bank= new Bank();
         bank.addRate("CHF", "USD", 2);
