5065322ed120dd73030e2b0102feeab7c09cb580
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-03-27 11:00:32 +0100

    p.60-63, issue #15

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -57,11 +57,30 @@ class MoneyTest
         assertEquals(Money.dollar(7), result);
     }
 
+    @Test
     public void testReduceMoney() {
         Bank bank= new Bank();
         Money result= bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
     }
 
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank= new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result= bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    /* pas censé marcher
+    @Test
+    public void testArrayEquals() {
+        assertEquals(new Object[] {"abc"}, new Object[] {"abc"});
+    }*/
+
+    @Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
+    }
 
 }
